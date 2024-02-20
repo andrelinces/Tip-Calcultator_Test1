c72fde8a688df611cf49ed02ca5e2b5310abb106
Andre Linces <andrelinces@yahoo.com.br>
2024-02-20 10:05:29 -0300

    Code inserted in the ThemeFont file.
    AvenirNext font configured.

--- Tip-Calcultator_Test1/config/ThemeFont.swift
+++ Tip-Calcultator_Test1/config/ThemeFont.swift
@@ -5,4 +5,21 @@
 //  Created by Andre Linces on 2/20/24.
 //
 
-import Foundation
+import UIKit
+
+struct ThemeFont {
+    //AvenirNext
+    
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func demibold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+}
